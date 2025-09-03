7c2fe5175d003dcf702db3f6b0f14f7328ffd4be
jhonlpjr <jhonlpjr@gmail.com>
2025-09-02 23:11:50 -0500

    Added Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,9 @@
 FROM mcr.microsoft.com/dotnet/sdk:9.0 AS build
 WORKDIR /src
 COPY . ./
-RUN dotnet restore
-RUN dotnet publish -c Release -o /app/publish
+WORKDIR /src/MovieApi
+RUN dotnet restore MovieApi.sln
+RUN dotnet publish MovieApi.sln -c Release -o /app/publish
 
 FROM mcr.microsoft.com/dotnet/aspnet:9.0 AS runtime
 WORKDIR /app
--- MovieApi/Program.cs
+++ MovieApi/Program.cs
@@ -3,6 +3,15 @@ using Infrastructure;
 using MovieApi.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy("AllowAll", policy =>
+	{
+		policy.AllowAnyOrigin()
+			  .AllowAnyHeader()
+			  .AllowAnyMethod();
+	});
+});
 
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
@@ -20,6 +29,7 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment()) { app.UseSwagger(); app.UseSwaggerUI(); }
 
+app.UseCors("AllowAll");
 app.UseMiddleware<ErrorHandlingMiddleware>();
 app.MapControllers();
 app.Run();
